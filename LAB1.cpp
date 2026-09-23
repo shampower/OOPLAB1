e91df209e5c01dc30fcd8ee44ab74bf6a788d340
shampower <danscham@yandex.ru>
2026-09-23 16:19:03 +0500

    обьявил фунции для работы с массивом

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-
+void fillArray(int (&arr)[10]);
+void printArray(const int (&arr)[10]);
+void swapElements(int (&arr)[10], int i, int j);
+void multiplyByTwo(int (&arr)[10]);
 int main()
 {
     return 0;
