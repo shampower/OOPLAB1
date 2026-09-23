4c7fd61326c945d1de6c4ee5d98c9f5e1cc61e91
shampower <danscham@yandex.ru>
2026-09-23 16:36:02 +0500

    создание и вывод массива + русский язык

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include <windows.h>
 
 void fillArray(int (&arr)[10]);
 void printArray(const int (&arr)[10]);
@@ -14,7 +16,44 @@ void fillArray(int (&arr)[10])
     }
 }
 
+void printArray(const int (&arr)[10])
+{
+    for (const auto& x : arr)
+    {
+        std::cout << x << " ";
+    }
+
+    std::cout << std::endl;
+}
+
+void swapElements(int (&arr)[10], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void multiplyByTwo(int (&arr)[10])
+{
+    for (int& x : arr)
+    {
+        x *= 2;
+    }
+}
+
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    srand(time(0));
+
+    int arr[10];
+
+    fillArray(arr);
+
+    std::cout << "Исходный массив:" << std::endl;
+    printArray(arr);
+
     return 0;
 }
