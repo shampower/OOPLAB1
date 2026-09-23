79677a2feb2e76a3738dc15cdb365035187079ab
shampower <danscham@yandex.ru>
2026-09-23 16:56:19 +0500

    это обработка первого отрицательного элемента

--- LAB12.cpp
+++ LAB12.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 #include <windows.h>
 
+
+void process(int*& arr, int size);
+
+void process(int*& arr, int size)
+{
+    int negativeIndex = -1;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < 0)
+        {
+            negativeIndex = i;
+            break;
+        }
+    }
+
+    if (negativeIndex == -1)
+    {
+        std::cout << "Отрицательных элементов нет." << std::endl;
+        return;
+    }
+
+    int newSize = negativeIndex;
+    int* newArr = new int[newSize];
+
+    for (int i = 0; i < newSize; i++)
+    {
+        newArr[i] = arr[i];
+    }
+
+    delete[] arr;
+    arr = newArr;
+
+    std::cout << "Первый отрицательный элемент находится под индексом: "
+              << negativeIndex << std::endl;
+}
+
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
