18d0f200b65f81f1832e458eb7c8df6d8e00fa5e
shampower <danscham@yandex.ru>
2026-09-23 16:21:57 +0500

    Реализовал заполнение массива

--- LAB1.cpp
+++ LAB1.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cstdlib>
+
 void fillArray(int (&arr)[10]);
 void printArray(const int (&arr)[10]);
 void swapElements(int (&arr)[10], int i, int j);
 void multiplyByTwo(int (&arr)[10]);
+
+void fillArray(int (&arr)[10])
+{
+    for (auto& x : arr)
+    {
+        x = rand() % 100;
+    }
+}
+
 int main()
 {
     return 0;
