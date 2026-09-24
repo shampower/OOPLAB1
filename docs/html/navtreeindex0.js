d3f00ad00acbad33ba20578bf62cc079d7e90fac
shampower <danscham@yandex.ru>
2026-09-24 14:00:27 +0500

    Добавил Doxygen документацию проекта

---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,12 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[0,0],
+"classes.html":[0,1],
+"functions.html":[0,2,0],
+"functions_vars.html":[0,2,1],
+"index.html":[],
+"pages.html":[],
+"struct_safe_array.html":[0,0,0],
+"struct_safe_array.html#a13dd58dae509770e4f24a710ec32587c":[0,0,0,0],
+"struct_safe_array.html#a95a3bc1efa9e8c6498eae6a5a4fdcc53":[0,0,0,1]
+};
---
+++ docs/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['safearray_0',['SafeArray',['../struct_safe_array.html',1,'']]],
+  ['size_1',['size',['../struct_safe_array.html#a95a3bc1efa9e8c6498eae6a5a4fdcc53',1,'SafeArray']]]
+];
